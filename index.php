3fff213d675983313b2134da9b820c72189d9854
hamzakhouzima <khouzimahamza20@gmail.com>
2022-11-17 21:06:54 +0100

    some progress

--- index.php
+++ index.php
@@ -4,157 +4,130 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-
     <title>Document</title>
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand me-4 " style="color:white;"  href="#">
-    <img src="music.jpg" width="40" height="40" alt="LOGO">
-</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" style="color:white;" href="#">Home</a>
-
-        </li>
-<div class="dropdown">
-        <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1"  data-bs-toggle="dropdown"  aria-expanded="false">
-		Dropdown button
-	    </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-            <li><a class="dropdown-item" href="#">Chordophones</a></li>
-            <li><a class="dropdown-item" href="#">Aerophones</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-	    </ul>
-</div>
-        <li class="nav-item">
-          <a class="nav-link" style="color:white;" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " style="color:white;" href="#">Disabled</a>
-        </li>
-      </ul>
+<!-- navbar -->
+<header >
+    
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"> <a class="navbar-brand" href="#" data-abc="true"><strong>Music</strong></span></a>
+        
+	
+ 
+ <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button> -->
+    
+   <div class="collapse navbar-collapse" id="navbarColor02">
+ 
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active"> <a class="nav-link" href="#" data-abc="true">Home <span class="sr-only">(current)</span></a> </li>
+            <li class="nav-item"> <a class="nav-link" href="#" data-abc="true">contact</a> </li>
+            <li class="nav-item"> <a class="nav-link" href="#" data-abc="true">Pricing</a> </li>
+            <li class="nav-item"> <a class="nav-link" href="#" data-abc="true">Social</a> </li>
+        </ul>
+        <form onsubmit="event.preventDefault()" class="form-inline my-2 my-lg-0"> <input class="form-control mr-sm-2" type="text" placeholder="Search"> <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button> </form>
     </div>
-  </div>
+   
 </nav>
+</header>
+<br>
 
-
-
-<div class="container-fluid">
-    <div class="row flex-nowrap">
-        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <span class="fs-5 d-none d-sm-inline">Menu</span>
-                </a>
-                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link align-middle px-0">
-                            <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                        <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Dashboard</span> </a>
-                        <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                            <!-- <li class="w-100"><a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1 </a></li>
-                            <li><a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2 </a></li> -->
-
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-0 align-middle">
-                            <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Orders</span></a>
-                    </li>
-                    <li>
-                        <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
-                            <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline">Bootstrap</span></a>
-                        <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-                            <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                            <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span> </a>
-                            <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                            <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 1</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 2</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 3</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 4</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-0 align-middle">
-                            <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span> </a>
-                    </li>
-                </ul>
-                <hr>
-                <div class="dropdown pb-4">
-                    <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="music.jpg" alt="hugenerd" width="30" height="30" class="rounded-circle">
-                        <span class="d-none d-sm-inline mx-1">loser</span>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                        <li><a class="dropdown-item" href="#">New project...</a></li>
-                        <li><a class="dropdown-item" href="#">Settings</a></li>
-                        <li><a class="dropdown-item" href="#">Profile</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Sign out</a></li>
-                    </ul>
-                </div>
+<div class="wrapper">
+    <br>
+    <div class="sidebar" >
+        
+    <button class="btn btn-success " style="width:99%;" data-toggle="modal" data-target="#exampleModal" >+New Instrument</button>
+    
+    
+    <ul>
+            <li><a href="#"><i class="fa fa-home"></i>Home</a></li>
+            <li><a href="#"><i class="fa fa-user"></i>Users</a></li>
+            <li><a href="#"><i class="fa fa-line-chart"></i>Timeline</a></li>
+            <li><a href="#"><i class="fa fa-comment-o"></i>Comments</a></li>
+            <li><a href="#"><i class="fa fa-gears"></i>Settings</a></li>
+            <li><a href="#"><i class="fa fa-address-book"></i>Contact</a></li>
+            <li><a href="#"><i class="fa fa-map-pin"></i>Map</a></li>
+        </ul> 
+        <div class="social_media">
+          <a href="#"><i class="fa fa-facebook-f"></i></a>
+          <a href="#"><i class="fa fa-twitter"></i></a>
+          <a href="#"><i class="fa fa-instagram"></i></a>
+      </div>
+    </div>
+    <div class="main_content">
+        <!-- <div class="header" style="background-color=white;"></div>    -->
+        <div class="info">
+   <!--------------------------------------------------------------------->
+   <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 mt-5">
+            <div class="col">
+                    <div class="card btn bg-white mb-3 p-0" href="#modal-product" data-bs-toggle="modal">
+                        <div style=" height: 300px; background-position: center; background-size: cover; background-repeat: no-repeat; background-image: url('music.jpg');"></div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center text-truncate fs-4 fw-bolder mb-3">Card title</h5>
+                            <p class="card-text text-start"><span class="fw-bold text-muted">Category:</span></p>
+                            <p class="card-text text-start"><span class="fw-bold text-muted">Quantity:</span></p>
+                            <p class="card-text text-start"><span class="fw-bold text-muted">Price:</span></p>
+                            <p class="card-text text-start"><span class="fw-bold text-muted">Description:</span></p>
+                        </div>
+                    </div>
             </div>
-        </div>
-        <div class="col py-3">
-        <button type="button" class="btn btn-primary btn-circle btn-xl text-right " data-toggle="modal" data-target="#exampleModalCenter">+Add new product</button>
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
+
+            <div class="col">
+                    <div class="card btn bg-white mb-3 p-0" href="#modal-product" data-bs-toggle="modal" >
+                        <div style="height: 300px; background-position: center; background-size: cover; background-repeat: no-repeat; background-image: url('music.jpg');"></div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center text-truncate fs-4 fw-bolder mb-3">Card title</h5>
+                            <p class="card-text text-start"><span class="fw-bold text-muted">Category:</span></p>
+                            <p class="card-text text-start"><span class="fw-bold text-muted">Quantity:</span></p>
+                            <p class="card-text text-start"><span class="fw-bold text-muted">Price:</span></p>
+                            <p class="card-text text-start"><span class="fw-bold text-muted">Description:</span></p>
+                        </div>
+                    </div>
+            </div>
+            
+            <div class="col">
+                    <div class="card btn bg-white mb-3 p-0" href="#modal-product" data-bs-toggle="modal">
+                        <div style="height: 300px; background-position: center; background-size: cover; background-repeat: no-repeat; background-image: url('music.jpg');"></div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center text-truncate fs-4 fw-bolder mb-3">Card title</h5>
+                            <p class="card-text text-start"><span class="fw-bold text-muted">Category:</span></p>
+                            <p class="card-text text-start"><span class="fw-bold text-muted">Quantity:</span></p>
+                            <p class="card-text text-start"><span class="fw-bold text-muted">Price:</span></p>
+                            <p class="card-text text-start"><span class="fw-bold text-muted">Description:</span></p>
+                        </div>
+                    </div>
+            </div>
+
+
+
+
+
+  
+
+
+      </div>
+    </div>
+    
+</div>
+<!----->
+
+
+
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalCenterTitle">Add an instrument</h5>
-        
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
-          
         </button>
       </div>
       <div class="modal-body">
-  
-      <div class="mb-md-5 mt-md-4 pb-5">
-            <div class="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" class="form-control form-control-lg" placeholder="Email"/>
-                <label class="form-label" for="typeEmailX" >Email</label>
-            </div>
-
-            <div class="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" class="form-control form-control-lg" placeholder="Email"/>
-                <label class="form-label" for="typeEmailX" >Email</label>
-            </div>
-            
-     
-<div>
+        
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -163,38 +136,27 @@
     </div>
   </div>
 </div>
-        </div>
-    </div>
-</div>
+
+
+ <!--------------------------------------------------------------------->
+
+<link rel="stylesheet" href="zwa9.css">
 
 
 
-<div class="card" id="card" style="height: 18rem;background-color:grey;">
-  <div class="card-body ">This is some text within a card body.</div>
-</div>
 
 
-<style>
-#logo{
- background-image:url("music.jpg");
-
-}
-
-#card{
-
-margin-right:500px;
-margin-top:200px;
-border-radius:20px;
-
-
-}
-
-</style>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
 </body>
+
+
+
+
+
+
+
+
 </html>
